ce8117f95b7517542c54818675463c45571b7c85
riyad1721 <reyadhasan7254@gmail.com>
2022-03-11 18:35:19 +0600

    Component cretion

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const num = 2015;
+  // const singer = {
+  //   name: "Chomok Hasan",
+  //   job: "Engineer"
+  // }
+  // const commonColor = {
+  //   color: 'black'
+  // }
   return (
     <div className="App">
-      <header className="App-header">
+      {/* { <header className="App-header">
+        <h3>Hello React I am Here</h3>
+        <p> Happy Happy React</p>
+        <div className="container">
+          <h2 style={commonColor}>This is Inside my Container</h2>
+          <p style={commonColor}>My number is : {num}</p>
+          <p style={{ color: 'black' }}>Singer : {singer.name} + ' ' + {singer.job}</p>
+          <p></p>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +33,27 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> } */}
+      <Person name="Md Reyad Hossain" Profession="Learning React js"></Person>
+      <Person name="Md Mazharul Islam" Profession="Production Engineer"></Person>
+      <Person name="Md Asif Iqbal Shuvo" Profession="BCS Cader"></Person>
+      <Person name="Rubayet Ahmed Rafsun" Profession="Play Boy"></Person>
+      <Person name="Roqunozzaman Emon" Profession="Every Girls Best Friends"></Person>
+
+
+
+
     </div>
   );
 }
 
+function Person(props) {
+  return (
+    <div className='person'>
+      <h1>Name : {props.name}</h1>
+      <h3> Profession : {props.Profession}</h3>
+    </div>
+
+  );
+}
 export default App;
